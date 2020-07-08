78e9a0c686a306cab6b9d25c9b29386548def7ae
Natalie Bravo <natalie.bravo@outlook.com>
2020-07-08 22:05:05 +0000

    sync functionalities

--- public/keyboard-listener.js
+++ public/keyboard-listener.js
@@ -5,6 +5,10 @@ export default function createKeyboardListener(document) {
         observers: []
     }
 
+    function registerPlayerId(playerId) {
+        state.playerId = playerId
+    }
+
     function subscribe(observerFunction) {
         state.observers.push(observerFunction)
     }
@@ -21,7 +25,8 @@ export default function createKeyboardListener(document) {
         const keyPressed = event.key
 
         const command = {
-            playerId: 'player1',
+            type: 'move-player',
+            playerId: state.playerId,
             keyPressed
         }
 
@@ -29,6 +34,7 @@ export default function createKeyboardListener(document) {
     }
 
     return {
-        subscribe
+        subscribe,
+        registerPlayerId
     }
 }
--- server.js
+++ server.js
@@ -13,16 +13,29 @@ app.use(express.static('public'))
 
 const game = createGame()
 
-//TEMP
-game.addPlayer({ playerId: 'player1', playerX: 0, playerY: 0 })
-game.addFruit({ fruitId: 'fruit1', fruitX: 5, fruitY: 5 })
-
+game.subscribe((command) => {
+    sockets.emit(command.type, command)
+})
 
 sockets.on('connection', (socket) => {
     const playerId = socket.id
     console.log(`> Player connected on Server with id: ${playerId}`)
 
+    game.addPlayer({ playerId })
+
     socket.emit('setup', game.state)
+
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId })
+        console.log(`> Player disconnected: ${playerId}`)
+    })
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId
+        command.type = 'move-player'
+
+        game.movePlayer(command)
+    })
 })
 
 server.listen(3000, () => {
